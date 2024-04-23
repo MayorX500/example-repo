909598ea79ec7eddb206cb66b21bb5c2c1b4b48c
Miguel Gomes <55558312+MayorX500@users.noreply.github.com>
2024-04-23 21:44:53 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,3 +5,4 @@ echo "This script will install the necessary packages for the project"
 echo "Installing the necessary packages"
 echo "BIP BOP BAP"
 echo "Done installing the necessary packages"
+mkdir /teste
