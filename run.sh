447ecad93d278b9a968b74e73cb9e04af1277276
Miguel Gomes <55558312+MayorX500@users.noreply.github.com>
2024-04-23 21:37:19 +0100

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Starting the script"
+echo "This script will install the necessary packages for the project"
+echo "Installing the necessary packages"
+echo "BIP BOP BAP"
+echo "Done installing the necessary packages"
